87657bb7e2b79bbb65a193aac8271be87cb5ed98
Fenty <185834709+feeenty@users.noreply.github.com>
2026-04-11 16:58:42 +0500

    tournaments_list/create_tournament

--- bot.py
+++ bot.py
@@ -3,10 +3,14 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from dotenv import load_dotenv
+from chyllonge.api import ChallongeApi
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHALLONGE_USER = os.getenv("CHALLONGE_USER")
+CHALLONGE_API_KEY = os.getenv("CHALLONGE_API_KEY")
 
+api = ChallongeApi()
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
@@ -16,9 +20,26 @@ async def start(msg: types.Message):
     await msg.answer("Привет!")
 
 
+@dp.message(Command("create_tournament"))
+async def create_tournament(msg: types.Message):
+    tournament = api.tournaments.create(name="T1", tournament_type="single elimination")
+    tournament_url = tournament["full_challonge_url"]
+    print(tournament["id"], tournament_url)
+    await msg.answer(tournament_url)
+
+
+@dp.message(Command("tournaments"))
+async def tournaments(msg: types.Message):
+    tournaments_data = api.tournaments.get_all()
+    for tournament in tournaments_data:
+        print(tournament)
+        await msg.answer(tournament)
+
+
 async def main():
-    print("Бот запущен...")
+    print("Бот запущен")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
